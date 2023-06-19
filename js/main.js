bc53633af50e079afaa1d2b87dfe7efb4076088e
marcocampisi <marco.campisi@outlook.it>
2023-06-19 16:56:17 +0200

    Add logic (complete js)

--- js/main.js
+++ js/main.js
@@ -8,3 +8,12 @@ const listaSpesa = [
 ];
 
 const ul = document.getElementById('ulList');
+
+let index = 0;
+
+while (index < listaSpesa.length) {
+    const newLiItem = document.createElement('li');
+    ul.append(newLiItem);
+    newLiItem.innerHTML = listaSpesa[index];
+    index++;
+}
